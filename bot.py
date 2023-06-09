dc90c40219a0ab37f640b37b1b621fb13da11cfd
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-06-09 15:25:46 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,71 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from aiogram.utils.markdown import hide_link
+from dotenv import load_dotenv
+
+from common import *
+
+ENV_FILE
+
+if os.path.exists(ENV_FILE):
+    load_dotenv('app.env')
+else: 
+    print('Отсутствует файл конфигурации!!')
+    quit()
+    
+bot = Bot(token=os.environ.get('yt_token'), parse_mode="HTML")
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start", "help"])
+async def start_command(message : types.Message):
+    await message.reply("Привет! Отправь мне ссылку на видеоролик YouTube и я пришлю тебе звуковую дорожку.")
+    if message.from_user.id not in user_id: write_user_history(userid=message.from_user.id, username=(message.from_user.full_name).strip(), users=user_id) 
+
+    # if is_tlg_user_allow(testuser=message.chat.id, username=message.from_user.username): 
+    #     await message.reply("Привет! Отправь мне ссылку на видеоролик YouTube и я пришлю тебе звуковую дорожку.")
+    # else: await bot.send_message(message.from_user.id, f'Уважаемый {message.from_user.full_name} для работы с данным сервисом пожалуйста обратитесь к администратору.')
+
+
+@dp.message_handler()
+async def send_audio(message : types.Message):
+    # print(message.from_user.username)
+    # if is_tlg_user_allow(testuser=message.chat.id, username=message.from_user.username): 
+    #     # await bot.send_message(message.from_user.id, message.text)
+    if message.from_user.id not in user_id: write_user_history(userid=message.from_user.id, username=(message.from_user.full_name).strip(), users=user_id) 
+    if "youtu" not in message.text:
+        await bot.send_message(message.from_user.id, 'Нет ссылки на ролик YouTube!')
+    else: 
+        # Кажется это хрень
+        track_title = await a_video_downloader(video_url=message.text)
+        await bot.send_message(message.chat.id, text=f"Видеофайл <b>'{track_title['title']}'</b> с YouTube успешно загружен! Приступаем к извлечению звуковой дорожки.")
+        # Кажется это хрень
+        audio_file = await a_audio_decoder(videofile=track_title['file'])
+        file_size = os.path.getsize(audio_file)
+        if file_size < MAX_SIZE_AUDIO:
+            # Загоняем трек в Shazam
+            track_definition = await recognize_track(input_file=audio_file)
+            with open(audio_file, 'rb') as audio:
+                await bot.send_audio(message.from_user.id, audio, caption=f"Аудиофайл видеоролика <b>'{track_title['title']}'</b>.")
+                # Отправляем пользователю автора песни и название трека из Shazam-а
+            if  track_definition != None:
+                user_text = f"Вы скачали видеоролик <b>'{track_title['title']}'</b>\n" + f"Название ролика - <b>{track_definition['title']}</b>\n" + f"Артист (группа) - <b>{track_definition['subtitle']}</b>"
+
+                if track_definition['image'] != None:
+                    await message.answer(
+                        f"{hide_link(track_definition['image'])}" + user_text
+                        )
+                else: await message.answer(user_text)
+                # Отправка текста песни - при наличии в Shazam-е
+                if track_definition['text'] != None:
+                    song_text = f"<b>{track_definition['subtitle']} - {track_definition['title']}</b>\n"
+                    for item in track_definition['text']:
+                        song_text += f"{item}\n" 
+                    await message.answer(song_text) 
+                                      
+        else: await bot.send_message(message.from_user.id, f"Аудиофайл видеоролика <b>'{track_title['title']}</b> имеет слишком большой размер для передачи! Попробуйте другой видеоролик.")
+
+
+if __name__ == "__main__":
+    read_user_history(user_id)
+    executor.start_polling(dp)
---
+++ common.py
@@ -0,0 +1,107 @@
+from pytube import YouTube
+import moviepy.editor
+from pathlib import Path
+import os, platform
+from shazamio import Shazam
+# import datetime
+from datetime import datetime
+
+
+
+telegram_bot_token = "YT_Bot_Token"
+user_id = []
+
+# Каталог для сохранения файлов
+WORK_FOLDER = 'data'
+# Максимальный размер файла для передачи через бот (поставил примерно)
+MAX_SIZE_AUDIO = 24117248
+# https://www.thepythoncode.com/article/make-a-youtube-video-downloader-in-python
+
+HISTORY_FILE = 'history.txt'
+ENV_FILE = 'app.env'
+
+async def a_video_downloader(video_url : str):
+    """
+    # the function takes the video url as an argument
+    """
+    # print("Вызов функции скачивания видео")
+    my_video = YouTube(url=video_url)
+    result = my_video.streams.get_lowest_resolution().download(WORK_FOLDER)
+    # print(result)
+    return {'title' : my_video.title, 'file' : result}
+
+async def a_audio_decoder(videofile : str):
+    """
+    the function takes the audio track from video file
+    """
+    # input(f"Извлекаем аудио из {videofile } ")
+    video_file = Path(videofile)
+    # input(video_file)
+    video = moviepy.editor.VideoFileClip(f'{video_file}')
+    audio = video.audio
+    audiofile = WORK_FOLDER + os.sep+ f"{video_file.stem}.mp3"
+    # input(audiofile)
+    audio.write_audiofile(audiofile)
+    video.close()
+    audio.close()
+    # Удаляем видеофайл
+    os.remove(videofile)
+    return audiofile
+
+
+async def recognize_track(input_file : str):
+    """
+    у меня функция заработала под Линуксом, и то только после
+    установки пакета ffmpeg
+    Под виндой что-то пошло не так - вероятно дело в моей криворукости
+    """
+    # Определяем платформу, где запущен скрипт
+    host = platform.system().lower()
+    if host == 'linux':
+        shazam = Shazam()
+        trackinfo = await shazam.recognize_song(input_file)
+        if trackinfo.get('track') != None:
+            result = {
+                'title' : trackinfo['track']['title'], 
+                'subtitle' : trackinfo['track']['subtitle'],
+                }
+            try:    # Ишем картинку
+                result['image'] = trackinfo['track']['images']['background']
+            except KeyError:
+                result['image'] = None
+            try:    # Ищем текст песни
+                result['text'] = trackinfo['track']['sections'][1]['text']
+            except KeyError:
+                result['text'] = None
+            return result
+        else: return None
+    else: return None
+
+    
+def read_user_history(users : list):
+        """
+        Считываем истрорию юзверей из файла HISTORY_FILE
+        """
+
+        if os.path.exists(HISTORY_FILE): 
+            f = open(file=HISTORY_FILE, mode="rt")
+            try:
+                lst = [str(item).replace("\n", "") for item in f.readlines()]
+            finally: f.close()
+
+            for item in lst:
+                newlst = item.split(":")
+                users.append(int(newlst[0])) 
+
+def write_user_history(userid : int, username: str, users : list):
+    users.append(userid)
+    try:
+        f = open(file=HISTORY_FILE, mode="at")
+        f.write(f"{userid}:{username}:{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+
+    finally: f.close()
+    
+
+
+
+
